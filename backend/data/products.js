bfa7b191134d209cfea001417b1d49bae92ed3a9
Harshwardhan Dhote <dhoteharsh5489@gmail.com>
2021-11-14 12:13:58 +0530

    my first commit react website

---
+++ backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "GTA 5",
+    imageUrl:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/271590/capsule_616x353.jpg?t=1618856444",
+    description:
+      "Grand Theft Auto V (also known as Grand Theft Auto Five, GTA 5, GTA V, or GTAV[2]) is a video game developed by Rockstar North. It is the fifteenth installment in the Grand Theft Auto series and the fifth game in the HD Universe title of the series.",
+    price: 1999,
+    countInStock: 15,
+  },
+  {
+    name: "Fortnite",
+    imageUrl:
+      "https://cdn1.epicgames.com/salesEvent/salesEvent/18BR_S18_Launcher_EGS_Blade_2560x1440_2560x1440-da6f6a137b90887357222b762c7675a1",
+    description:
+      "Fortnite is an online video game developed by Epic Games and released in 2017. It is available in three distinct game mode versions that otherwise share the same general gameplay and game engine: Fortnite: Save the World, a cooperative hybrid-tower defense-shooter-survival game for up to four players to fight off zombie-like creatures and defend objects with traps and fortifications they can build; Fortnite Battle Royale, a best in class royale game in which up to 100 players fight to be the last person standing; and Fortnite Creative, in which players are given complete freedom to create worlds and battle arenas.",
+    price: 999,
+    countInStock: 10,
+  },
+  {
+    name: "League of Legends",
+    imageUrl:
+      "https://cdn1.dotesports.com/wp-content/uploads/2019/09/12195522/league-of-legends.jpg",
+    description:
+      "League of Legends (LoL), commonly referred to as League, is a 2009 multiplayer online battle arena video game developed and published by Riot Games. Inspired by Defense of the Ancients, a custom map for Warcraft III, Riot's founders sought to develop a stand-alone game in the same genre. Since its release in October 2009, the game has been launched at Rs.1299 and is monetized through purchasable character customization.",
+    price: 1299,
+    countInStock: 5,
+  },
+  {
+    name: "Valorant",
+    imageUrl:
+      "https://cdn.mos.cms.futurecdn.net/E56FpaSWYShMwByomeiN43.png",
+    description:
+      "Valorant (stylized as VALORANT) is a first-person hero shooter developed and published by Riot Games, for Microsoft Windows. First teased under the codename Project A in October 2019, the game began a closed beta period with limited access on April 7, 2020, followed by an official release on June 2, 2020. The development of the game started in 2014.",
+    price: 1499,
+    countInStock: 25,
+  },
+  {
+    name: "CS:go",
+    imageUrl:
+      "https://img.cncenter.cz/foto/counter-strike-csgo-crop/Zml0LWluLzEwNTB4OTk5OS9maWx0ZXJzOnF1YWxpdHkoODUpOm5vX3Vwc2NhbGUoKS9pbWc/6264241.jpg?v=0&st=aiZCpyHhJUILn0naf13OkEiKrqK4JABXa8n25LvCzjU&ts=1600812000&e=0",
+    description:
+      "Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series. Developed for over two years, Global Offensive was released for Windows, macOS, Xbox 360, and PlayStation 3 in August 2012, and for Linux in 2014. Valve still regularly updates the game, both with smaller balancing patches and larger content additions.",
+    price: 1499,
+    countInStock: 4,
+  },
+  {
+    name: "DOTA 2",
+    imageUrl:
+      "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota2_social.jpg",
+    description:
+      "Dota 2 is an Action RTS game, developed by Valve Corporation. The title was formally announced on October 13, 2010; and was released as at Rs.999 on July 9th, 2013 for Windows, and July 18th, 2013 for Mac OS and Linux. It is the successor to the very popular Warcraft 3 mod, Defense of the Ancients, which was based on the Aeon of Strife map for StarCraft.",
+    price: 999,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
